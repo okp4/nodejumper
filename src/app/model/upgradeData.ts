2f3dc983aa190c095e2663a9fa495799d8f41beb
vbloher <vbloher7@gmail.com>
2022-08-19 15:55:47 +0700

    add possibility to disable automatic chain upgrades

--- src/app/model/upgradeData.ts
+++ src/app/model/upgradeData.ts
@@ -2,9 +2,11 @@ export class UpgradeData {
   height: number;
   version: string;
   manualScriptContent?: string;
+  automaticUpgradeDisabled?: boolean;
 
-  constructor(height: number, version: string) {
+  constructor(height: number, version: string, automaticUpgradeDisabled: boolean) {
     this.height = height;
     this.version = version;
+    this.automaticUpgradeDisabled = automaticUpgradeDisabled;
   }
 }
